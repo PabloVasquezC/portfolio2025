25e2eb66612b222d716eb1a457abbc9246eb5973
PabloVasquez <vascor.pablo@gmail.com>
2025-01-22 17:52:01 -0300

    card v1

--- src/components/ProjectsSection/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectsSection/ProjectCard/ProjectCard.tsx
@@ -2,12 +2,14 @@ import Project from "./Project";
 
 export default function ProjectCard( {project}: {project: Project} ) {
     return (
-        <div className="flex justify-center items-center h-screen bg-gray-800">
-            <h1>{project.title}</h1>
-            <img src={project.image} alt="project-image" />
-            <p>{project.description}</p>
+        <div className="flex-col justify-center items-center h-96 w-96 m-4 ">
+            <h1 className="text-2xl text-center">{project.title}</h1>
 
-            <button>Ver</button>
+            <div className="flex-col justify-center items-center  bg-gray-800">
+                <img src={project.image} alt="project-image" />
+                <p>{project.description}</p>
+                <button >Ver</button>
+            </div>
         </div>
     )
 }
