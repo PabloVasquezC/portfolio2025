83cbad9a9d57af5b31056542e4365e4913c183c9
PabloVasquez <vascor.pablo@gmail.com>
2025-01-22 12:32:57 -0300

    changing skill V1

--- src/components/About/AboutSection.tsx
+++ src/components/About/AboutSection.tsx
@@ -5,11 +5,8 @@ export default function AboutSection() {
     return (
     
             <div className='flex-col justify-center items-center h-screen'>
-                <h1 className='text-2xl text-center'>Sobre mi</h1>
+                <h1 className='text-3xl text-center '>Sobre mi</h1>
                 <div className="text-center">"Mi nombre es Pablo, Soy principalmente programador web, aunque he incursionado en programación móvil, proyectos de machine learning, etc."</div>
-
             </div>
-
-        
     )
 }
--- src/components/HomePage.tsx/HomePage.tsx
+++ src/components/HomePage.tsx/HomePage.tsx
@@ -1,14 +1,36 @@
+import { useState, useEffect, useMemo } from "react";
 import AboutSection from "../About/AboutSection";
 import ProjectsSection from "../ProjectsSection/ProjectsSection";
 
 export default function HomePage() {
+    // Memoriza el array para que no cambie entre renders
+    const mySkillList = useMemo(() => ['Frontend', 'Backend', 'Mobile', 'Machine Learning', 'DevOps', 'UI/UX'], []);
+
+    const [mySkill, setMySkill] = useState<string>(mySkillList[0]); // Inicializa con la primera habilidad
+
+    useEffect(() => {
+        let index = 0;
+
+        const interval = setInterval(() => {
+            index = (index + 1) % mySkillList.length; // Alterna entre las habilidades de la lista
+            setMySkill(mySkillList[index]);
+        }, 3000);
+
+        return () => clearInterval(interval); // Limpia el intervalo al desmontar el componente
+    }, [mySkillList]); // Depende de la lista memorizada
+
     return (
-        <div className="">
-            <div className='flex justify-center items-center h-screen'>
-                <h1 className='text-2xl'>Hola, Soy Pablo Vásquez</h1>
+            <div>
+                <div className="flex-col justify-center items-center h-[1500px] ">
+                <h1 className="text-3xl text-center">¡Hola, Soy Pablo Vásquez!</h1>
+                <br />
+                <h1 className="text-3xl text-center">Desarrollador {mySkill}</h1>
+                
             </div>
             <AboutSection />
             <ProjectsSection />
-        </div>
-    )
+            </div>
+            
+        
+    );
 }
