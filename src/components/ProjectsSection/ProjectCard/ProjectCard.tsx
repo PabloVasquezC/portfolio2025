3caf57a65ee576543ef3a8c44668f5b71c9bf43f
PabloVasquez <vascor.pablo@gmail.com>
2025-01-22 22:44:46 -0300

    solving import error

--- src/components/ProjectsSection/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectsSection/ProjectCard/ProjectCard.tsx
@@ -4,11 +4,10 @@ export default function ProjectCard( {project}: {project: Project} ) {
     return (
         <div className="flex-col justify-center items-center h-96 w-96 m-4 ">
             <h1 className="text-2xl text-center">{project.title}</h1>
-
             <div className="flex-col justify-center items-center  bg-gray-800">
                 <img src={project.image} alt="project-image" />
-                <p>{project.description}</p>
-                <button >Ver</button>
+                <p className="text-white text-center">{project.description}</p>
+                <button className="bg-white text-black">Ver</button>
             </div>
         </div>
     )
