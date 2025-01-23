d8d34e701c8d70df3c0b4656c327a999f6312410
PabloVasquez <vascor.pablo@gmail.com>
2025-01-23 09:50:57 -0300

    card v2

--- src/components/ProjectsSection/ProjectsData.ts
+++ src/components/ProjectsSection/ProjectsData.ts
@@ -1,22 +1,39 @@
 import Project from "./ProjectCard/Project";
 import CoffeeFamily from "../../../assets/coffee-family.png";
 import GameWorld from "../../../assets/gameworld.png";
+import ScrabbeMath from "../../../assets/scrabblemath.png";
+import DjangoLibrary from "../../../assets/djangoLibrary.png";
 
 const ProjectsData: Project[] = [
     {
         title: "Coffee Family",
-        description: "Description 1",
+        description: "Apliación web para la gestión de una cafetería, con funcionalidades de administración de productos, ventas y usuarios.",
         image: CoffeeFamily,
         url: "https://familycoffee.vercel.app/",
         github: "https://github.com/PabloVasquezC/coffee-family-back"
     },
     {
         title: "Gameworld",
-        description: "Description 2",
+        description: "Plataforma de juegos en línea, con funcionalidades de registro de usuarios, selección de juegos y ranking de jugadores.",
         image: GameWorld,
         url: "https://gameworldv2.vercel.app/",
         github: "https://www.github.com"
+    },
+    {
+        title: "Scrabble Math",
+        description: "Juego educativo para la práctica de operaciones matemáticas básicas, con funcionalidades de registro de usuarios y ranking de puntajes.",
+        image: ScrabbeMath,
+        url: "https://scrabblemath.netlify.app/",
+        github: "https://www.github.com"
+    },
+    {
+        title: "Django Library",
+        description: "Aplicación web para la gestión de una biblioteca, con funcionalidades de administración de libros, autores y préstamos.",
+        image: DjangoLibrary,
+        url: "https://elpoetalibrary-ayfjfdashtdeepcd.canadacentral-01.azurewebsites.net/",
+        github: ""
     }
+
 ];
 
 export default ProjectsData;
