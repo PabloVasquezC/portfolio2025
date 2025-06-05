152ad510912ffae1e55ed1ce2e052c39b51b2e8e
Pablo Vasquez <vascor.pablo@gmail.com>
2025-06-05 17:16:17 -0400

    Particle Background and ProfileCard implemented

--- src/components/myOwnComponents/HomePage.tsx/HomePage.tsx
+++ src/components/myOwnComponents/HomePage.tsx/HomePage.tsx
@@ -4,6 +4,7 @@ import NavBar from "../NavBar/NavBar";
 import ProjectsSection from "../ProjectsSection/ProjectsSection";
 import SplashCursor from '../../reactBitsComponents/SplashCursor/SplashCursor';
 import './HomePage.css';
+import MovingParticleBackground from "../../reactBitsComponents/MovingParticleBackground/MovingParticleBackground";
 
 export default function HomePage() {
   const [darkMode, setDarkMode] = useState(true);
@@ -15,16 +16,46 @@ export default function HomePage() {
 
   return (
     <>
+      {/* Fondo fijo detrás de todo */}
+      <div style={{ 
+        width: '100vw', 
+        height: '100vh', 
+        position: 'fixed', 
+        top: 0, 
+        left: 0, 
+        backgroundColor: darkMode ? '#000' : '#fff', 
+        zIndex: -1,
+        overflow: 'hidden' 
+      }}>
+        <MovingParticleBackground
+          particleColors={['#ffffff', '#ffffff']}
+          particleCount={200}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
+      </div>
+
+      {/* Cursor animado */}
       <SplashCursor />
+
+      {/* NavBar con cambio de tema */}
       <NavBar onDarkModeChange={handleDarkModeChange} darkMode={darkMode} />
-      <div className={`container-fluid home ${darkMode ? "dark" : "light"}`}>
-      <div id="inicio">
+
+      {/* Contenido que se superpone al fondo */}
+      <div className={`container-fluid home ${darkMode ? "dark" : "light"}`} style={{ position: 'relative', zIndex: 1 }}>
+        <div id="inicio">
           <AboutSection />
         </div>
         <div id="proyectos">
           <ProjectsSection />
         </div>
-        <div id="formacion"></div>
+        <div id="formacion">
+          {/* Puedes agregar aquí tu sección de formación */}
+        </div>
       </div>
     </>
   );
